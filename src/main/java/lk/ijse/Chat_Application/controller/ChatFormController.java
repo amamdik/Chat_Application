b689ce67fb9199f537e197ee05695e7e9c6ea125
unknown <amamdik@gmail.com>
2023-07-13 20:22:51 +0530

    ChatForm Design

--- src/main/java/lk/ijse/Chat_Application/controller/ChatFormController.java
+++ src/main/java/lk/ijse/Chat_Application/controller/ChatFormController.java
@@ -1,4 +1,11 @@
 package lk.ijse.Chat_Application.controller;
 
+import javafx.event.ActionEvent;
+
 public class ChatFormController {
+    public void enterSendOnAction(ActionEvent event) {
+    }
+
+    public void sendOnAction(ActionEvent event) {
+    }
 }
