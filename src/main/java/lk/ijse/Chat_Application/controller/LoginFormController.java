12ace62ea8d6e13ed455f8401b1d902d7fc89e29
unknown <amamdik@gmail.com>
2023-07-13 18:57:44 +0530

    LoginForm Design

--- src/main/java/lk/ijse/Chat_Application/controller/LoginFormController.java
+++ src/main/java/lk/ijse/Chat_Application/controller/LoginFormController.java
@@ -1,4 +1,11 @@
 package lk.ijse.Chat_Application.controller;
 
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public void nameOnAction(ActionEvent event) {
+    }
+
+    public void loginOnAction(ActionEvent event) {
+    }
 }
